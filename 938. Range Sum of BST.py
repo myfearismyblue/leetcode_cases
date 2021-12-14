830ba2134154361197e4024adc6b578f8a0df336
myfearismyblue <myfearismyblue@gmail.com>
2021-12-14 22:34:19 +0300

    Added gitignore

--- 938. Range Sum of BST.py
+++ 938. Range Sum of BST.py
@@ -12,10 +12,12 @@ class TreeNode:
 class Solution:
     def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
         recur_this = root.val if low <= root.val <= high else 0
-        recur_left = self.rangeSumBST(root.left, low, high) if  low <= self.rangeSumBST(root.left, low,
-                                                                                             high) <= high else 0
-        recur_right = self.rangeSumBST(root.right, low, high) if low <= self.rangeSumBST(root.right, low,
-                                                                                            high) <= high else 0
+        recur_left, recur_right = 0, 0
+        if root.val != None
+            recur_left = self.rangeSumBST(root.left, low, high) if low <= self.rangeSumBST(root.left, low,
+                                                                                                 high) <= high else 0
+            recur_right = self.rangeSumBST(root.right, low, high) if low <= self.rangeSumBST(root.right, low,
+                                                                                                high) <= high else 0
         answer = recur_this + recur_left + recur_right
 
         return answer
