2f93cf5b24cbf3fbd6dbe09e6653f20c78d1c8ab
myfearismyblue <myfearismyblue@gmail.com>
2021-12-15 21:41:47 +0300

    Initial for #214

---
+++ 214. Shortest Palindrome.py
@@ -0,0 +1,12 @@
+# You are given a string s. You can convert s to a palindrome by adding characters in front of it.
+#
+# Return the shortest palindrome you can find by performing this transformation.
+
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        pass
+
+my_sol = Solution()
+
+s = ''
+print(my_sol.shortestPalindrome(s))
