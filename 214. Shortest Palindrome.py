8b5e7e5d4f78279c5f48bb0a98bbc23376abf6d0
myfearismyblue <myfearismyblue@gmail.com>
2021-12-16 21:40:51 +0300

    Refactoring

--- 214. Shortest Palindrome.py
+++ 214. Shortest Palindrome.py
@@ -4,37 +4,44 @@
 
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        # The common formula of such palindrome: BxOyA, where xOyA == s, and x - is the 'root',
-        # y == x[::-1] - mirrored part of the root of initial string, O - is a sliding pointer iterating through
-        # [s[0], '', s[1], ''....], B == A[::-1],
-        # A - is a remaining part
-        # For any string s, at least x == '', O == s[0], y == '', A == s[1:]
-        answer = s[::-1] + s
-        for idx in range(0, len(s) // 2 + 1):                           # FIXME: check ranges
-            # idx is the sliding pointer of O
+        # The common formula of such palindrome: BxOyA, where xOyA == s, and x - is the left root,
+        # y == left_root[::-1] -
+        # mirrored part of the root (right), O - is a sliding middle pointer iterating through
+        # [s[0], '', s[1], ''....],
+        # A - is a remaining suffix of the string
+        # B == remaining_suffix[::-1],
+        answer = s[::-1] + s                                    # at least
+        if not s:
+            return answer
+        for idx in range(0, len(s) // 2 + 1):
+            # idx is the sliding pointer of middle
 
-            # The first case: O points to an char
-            O = s[idx]
-            x = s[:idx]
-            y1 = s[idx+1:idx+1+len(x)]
-            if x == y1[::-1]:
-                y = y1
-                A = s[idx + 1 + len(x):]
-                answer = A[::-1] + x + O + y + A if len(A[::-1] + x + O + y + A) < len(answer) else answer
+            # The first case: middle_pointer points to an char
+            middle_pointer = s[idx]
+            left_root = s[:idx]
+            y1 = s[idx+1:idx+1+len(left_root)]
+            if left_root == y1[::-1]:
+                right_root = y1
+                remaining_suffix = s[idx + 1 + len(left_root):]
+                answer = remaining_suffix[::-1] + left_root + middle_pointer + right_root + remaining_suffix \
+                    if len(remaining_suffix[::-1] + left_root + middle_pointer +
+                           right_root + remaining_suffix) < len(answer) else answer
 
-            # The second case: O points to '' between chars
-            O = ''
-            x = s[:idx+1]
-            y2 = s[idx + 1:idx + 1 + len(x)]
-            if x == y2[::-1]:
-                y = y2
-                A = s[idx + 1 + len(x):]
-                answer = A[::-1] + x + O + y + A if len(A[::-1] + x + O + y + A) < len(answer) else answer
+            # The second case: middle_pointer points to '' between chars
+            middle_pointer = ''
+            left_root = s[:idx+1]
+            y2 = s[idx + 1:idx + 1 + len(left_root)]
+            if left_root == y2[::-1]:
+                right_root = y2
+                remaining_suffix = s[idx + 1 + len(left_root):]
+                answer = remaining_suffix[::-1] + left_root + middle_pointer + right_root + remaining_suffix \
+                    if len(remaining_suffix[::-1] + left_root + middle_pointer +
+                           right_root + remaining_suffix) < len(answer) else answer
 
         return answer
 
 
 my_sol = Solution()
 
-s = 'a'
+s = 'aaccf'
 print(my_sol.shortestPalindrome(s))
