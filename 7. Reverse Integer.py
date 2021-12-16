e80e72bfc98f1ec608df4f5616bcc9cffa05d7bb
myfearismyblue <myfearismyblue@gmail.com>
2021-12-16 22:48:56 +0300

    Implementation for #7

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -4,8 +4,12 @@
 # Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
 class Solution:
     def reverse(self, x: int) -> int:
-        pass
+        sign = 1 if x >= 0 else -1
+        x = abs(x)
+        reverse = sign * int(str(x)[::-1])
+        return reverse if -2 ** 31 <= reverse <= 2 ** 31 - 1 else 0
+
 
 x = 123
-my_sol  = Solution()
+my_sol = Solution()
 print(my_sol.reverse(x))
