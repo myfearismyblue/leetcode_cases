88a7f250cf51ee960cab51875004d015ccc5f09c
Andrey Smekhov <myfearismyblue@gmail.com>
2021-12-20 20:40:53 +0300

    Initial commit for #1200

---
+++ 1200. Minimum Absolute Difference.py
@@ -0,0 +1,16 @@
+# Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
+#
+# Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+#
+# a, b are from arr
+# a < b
+# b - a equals to the minimum absolute difference of any two elements in arr
+
+class Solution:
+    def minimumAbsDifference(self, arr):
+        pass
+
+
+my_sol = Solution()
+arr = [4, 2, 1, 3]
+print(my_sol.minimumAbsDifference(arr))
