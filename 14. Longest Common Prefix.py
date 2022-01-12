d20bc748a273f37e2da59604cc79231bd5447333
Andrey Smekhov <myfearismyblue@gmail.com>
2022-01-12 22:03:34 +0300

    Fixing bug w/ returning all common leteers at the same positions for #14

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -9,17 +9,23 @@ class Solution:
         for idx in range(201):
             try:
                 letter_set = set(strs[0][idx])
-                for word in strs:
-                    assert word.islower()
-                    letter_set.add(word[idx])
-                if len(letter_set) == 1:
-                    answer.append(word[idx])
             except IndexError:
                 return ''.join(answer)
+
+            for word in strs:
+                assert word.islower()
+                letter_set.add(word[idx])
+            if len(letter_set) == 1:
+                answer.append(word[idx])
+            elif len(letter_set) > 1:
+                return ''.join(answer)
+            else:
+                assert True, "Impossible: if the letter set is empty, exception should be caught"
+
 
         return ''.join(answer)
 
 strs = ["flower", "flow", "flight"]
-strs = ['', '']
+# strs = ['car', 'cir']
 my_sol = Solution()
 print(my_sol.longestCommonPrefix(strs))
