d633298f4a312f67d259de55431a7432651a449c
Andrey Smekhov <myfearismyblue@gmail.com>
2022-01-12 21:50:53 +0300

    Initial and implementation for #14

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,24 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+#
+# If there is no common prefix, return an empty string "".
+
+class Solution:
+    def longestCommonPrefix(self, strs) -> str:
+        answer = []
+        letter_set = set()
+        for idx in range(201):
+            try:
+                letter_set = set(strs[0][idx])
+                for word in strs:
+                    letter_set.add(word[idx])
+                if len(letter_set) == 1:
+                    answer.append(word[idx])
+            except IndexError:
+                return ''.join(answer)
+
+        return ''.join(answer)
+
+strs = ["flower", "flow", "flight"]
+strs = ['', '']
+my_sol = Solution()
+print(my_sol.longestCommonPrefix(strs))
