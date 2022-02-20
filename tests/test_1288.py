64fcf7358f5f266aac8ab6406787f9fd8d13403b
Andrey Smekhov <myfearismyblue@gmail.com>
2022-02-20 20:56:52 +0300

    Initial for #1288 w/ pytest

---
+++ tests/test_1288.py
@@ -0,0 +1,18 @@
+# Example 1:
+#
+# Input: intervals = [[1,4],[3,6],[2,8]]
+# Output: 2
+# Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+# Example 2:
+#
+# Input: intervals = [[1,4],[2,3]]
+# Output: 1
+
+# 1 <= intervals.length <= 1000
+# intervals[i].length == 2
+# 0 <= li <= ri <= 105
+# All the given intervals are unique.
+
+from _1288_Remove_Covered_Intervals import Solution
+import pytest
+
